d16502fb9f9780ba0f21f0bfaed4b3d260d52e86
Allen <almcbroombey@hotmail.com>
2022-03-12 15:49:49 -0500

    create quiz questions for js file

---
+++ docs/assets/js/script.js
@@ -0,0 +1,41 @@
+var questions = [
+    {
+        question-1: "How can JavaScript display data?",
+        choices: { a: "Writing into an HTML element, using innerHTML.",
+            b: "Writing into an alert box, using window.alert'()'."
+            c: "Writing into the browser console, using console.log'()'." },
+            d: "all of the above."
+    },
+
+    {
+        question-2: "How can JavaScript display data?",
+        choices: { a: "Writing into an HTML element, using innerHTML.",
+            b: "Writing into an alert box, using window.alert'()'."
+            c: "Writing into the browser console, using console.log'()'." },
+            d: "all of the above."
+    },
+
+    {
+        question-3: "How can JavaScript display data?",
+        choices: { a: "Writing into an HTML element, using innerHTML.",
+            b: "Writing into an alert box, using window.alert'()'."
+            c: "Writing into the browser console, using console.log'()'." },
+            d: "all of the above."
+    },
+
+    {
+        question-4: "How can JavaScript display data?",
+        choices: { a: "Writing into an HTML element, using innerHTML.",
+            b: "Writing into an alert box, using window.alert'()'."
+            c: "Writing into the browser console, using console.log'()'." },
+            d: "all of the above."
+    },
+
+    {
+        question-5: "How can JavaScript display data?",
+        choices: { a: "Writing into an HTML element, using innerHTML.",
+            b: "Writing into an alert box, using window.alert'()'."
+            c: "Writing into the browser console, using console.log'()'." },
+            d: "all of the above."
+    },
+]
